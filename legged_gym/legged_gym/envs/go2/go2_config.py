2c9f9dba60a04bf794320b5ead2ae8262dd1f323
sivan <1309519635@qq.com>
2026-02-02 16:56:31 +0800

    增加tracking_pitch奖励函数 步态正常 terrain_level上升很快

--- legged_gym/legged_gym/envs/go2/go2_config.py
+++ legged_gym/legged_gym/envs/go2/go2_config.py
@@ -187,7 +187,7 @@ class Go2RoughCfg( LeggedRobotCfg ):
             # tracking rewards
             tracking_goal_vel = 2.5
             tracking_ang_vel = 0.5
-            # tracking_pitch = 0.5
+            tracking_pitch = 0.5
             # tracking_lin_vel = 0.25
             # tracking_yaw = 0.05
             # regularization rewards
